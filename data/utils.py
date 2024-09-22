4db5bc7a720ee97c4fccd6a50c4eeb0e12316295
zhanghaoyu <15981853295@163.com>
2024-09-22 17:22:48 +0800

    第一问预测结果

--- data/utils.py
+++ data/utils.py
@@ -1,17 +1,73 @@
 import pandas as pd
 import numpy as np
-import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 
+def get_columns(file_path, star_coloumn_id):
+    """
+    返回筛选的列
+    :file_path 使用文件相对路径
+    """
+    # file_path = '附件一（训练集）.xlsx' 
+    df1 = pd.read_excel(file_path, sheet_name="材料1")
+    selected_columns1 = df1.iloc[:, star_coloumn_id:]
 
+    df2 = pd.read_excel(file_path, sheet_name="材料2")
+    selected_columns2 = df2.iloc[:, star_coloumn_id:]
+
+    df3 = pd.read_excel(file_path, sheet_name="材料3")
+    selected_columns3 = df3.iloc[:, star_coloumn_id:]
+
+    df4 = pd.read_excel(file_path, sheet_name="材料4")
+    selected_columns4 = df4.iloc[:, star_coloumn_id:]
+
+    return selected_columns1, selected_columns2, selected_columns3, selected_columns4
+    
 def get_labels(row):
     """
-    转换为标签数据
+    转换为标签数据 one-hot向量形式
     """
     one_hot_labels = pd.get_dummies(row)
     return one_hot_labels
+
+def get_onehot_labels(file_path, name):
+    """
+    输入文件路径，返回onehot形式标签
+    """
+    df = pd.read_excel(file_path, sheet_name=name)
+    labels = df["励磁波形"]
+    onehot_labels = get_labels(labels)
+    return onehot_labels
+
+def get_predict_clolumns(file_path):
+
+    # 读取数据
+    df = pd.read_excel(file_path)
+
+    # 筛选第四列为‘材料1’的行x
+    filtered_df = df[df.iloc[:, 3] == '材料1']
+    # 选择第五列及其后面的所有列
+    result1 = filtered_df.iloc[:, 4:]
+
+    filtered_df = df[df.iloc[:, 3] == '材料2']
+    # 选择第五列及其后面的所有列
+    result2 = filtered_df.iloc[:, 4:]
+
+    filtered_df = df[df.iloc[:, 3] == '材料3']
+    # 选择第五列及其后面的所有列
+    result3 = filtered_df.iloc[:, 4:]
+
+    filtered_df = df[df.iloc[:, 3] == '材料4']
+    # 选择第五列及其后面的所有列
+    result4 = filtered_df.iloc[:, 4:]
+
+    return result1, result2, result3, result4
+
 
 
 def get_mean(row):
@@ -57,6 +113,57 @@ def labels(row):
     le = LabelEncoder()
     rows = le.fit_transform(rows)
     return rows
+
+
+class RandomForest():
+    def __init__(self, train_file_path, predict_file_path, train_start_column_id, predict_start_column_id):
+        self.train_file_path = train_file_path
+        self.predict_file_path = predict_file_path
+
+        self.train_start_column_id = train_start_column_id
+        self.predict_start_column_id = predict_start_column_id
+
+    def train(self, selected_columns, name):
+        train_file_path = self.train_file_path
+        start_column_id = self.train_start_column_id
+        
+        # 获取特征
+        mean = get_mean(selected_columns)
+        std = get_std(selected_columns)
+        max_values = max(selected_columns)
+        min_values = min(selected_columns)
+
+        # 训练标签
+        Y = get_onehot_labels(train_file_path, name)
+        X = np.column_stack((max_values, min_values, std))
+
+        # 拆分数据集为训练集和测试集
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+        # 训练模型
+        rf.fit(X_train, y_train)
+
+        # 评估模型
+        accuracy = rf.score(X_test, y_test)
+        print(f"附件一测试集模型准确率: {accuracy:.2f}")
+        y_pred = rf.predict(X_test)
+        print(classification_report(y_test, y_pred))
+        return rf
+    
+    def predict(self, train_selected_columns, predict_columns, name):
+        rf = self.train(train_selected_columns, name)
+        # 获取特征
+        mean = get_mean(predict_columns)
+        std = get_std(predict_columns)
+        max_values = max(predict_columns)
+        min_values = min(predict_columns)
+
+        X = np.column_stack((max_values, min_values, std))
+        y_pred = rf.predict(X)
+
+        return y_pred
+        # 预测
 
 
 
